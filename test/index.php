e25bd0ccaca2fd6c51faff11342c530d7e5f7333
AndryRio <112557896+AndryRio@users.noreply.github.com>
2023-09-25 15:36:36 +0200

    Add files via upload

--- test/index.php
+++ test/index.php
@@ -12,17 +12,49 @@
     </h1>
     <?php
 
-        $firstName = 'Gio';
+        /*$firstName = 'Gio';
+
+        $firstName = 'Joe';
 
         $x = 1;
         $y = &$x;
 
         $x = 3;
 
-        //echo $y;
+        echo $y;
 
 
         echo 'Hello '.$firstName ;
+
+        // CONSTANTS
+
+        define('STATUS_PAID', 'paid');
+
+        echo defined('STATUS_PAID');
+
+        const STATUS_PAID = 'paid';
+
+        if(true) {
+            define('STATUS_PAID', 9);
+        }
+
+        $paid = 'PAID';
+        
+        define('STATUS_'. $paid, $paid);
+
+        echo STATUS_PAID;
+
+        echo PHP_VERSION;
+
+        echo __FILE__;*/
+
+        // VARIABLE VARIABLES
+
+        $foo = 'bar';
+
+        $$foo = 'baz';
+        
+        echo $foo, $$foo;
 
         ?>
     
