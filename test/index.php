5a391e2e5160eef6238e0d8b838ace34e11551ea
AndryRio <112557896+AndryRio@users.noreply.github.com>
2023-09-26 17:59:24 +0200

    Add files via upload

--- test/index.php
+++ test/index.php
@@ -26,7 +26,7 @@
 
         echo 'Hello '.$firstName ;
 
-        // CONSTANTS
+    // CONSTANTS
 
         define('STATUS_PAID', 'paid');
 
@@ -46,15 +46,64 @@
 
         echo PHP_VERSION;
 
-        echo __FILE__;*/
+        echo __FILE__;
 
-        // VARIABLE VARIABLES
+    // VARIABLE VARIABLES
 
         $foo = 'bar';
 
         $$foo = 'baz';
         
         echo $foo, $$foo;
+
+    // Data Types & Type Casting
+
+    # 4 Scalar Types
+        # bool - true/false;
+        $completed = true;
+        # int - 1, 2, 3, 0, -4
+        $score = 75;
+        #float - 1.4, 0.2, 0.002, -15.43
+        $price = 0.99;
+        #string - "Gio", "Hello World"
+        $greeting = "Hello Gio";
+
+        echo $completed . '<br />';
+        echo $score . '<br />';
+        echo $price . '<br />';
+        echo $greeting . '<br />';
+
+        var_dump($completed);
+        echo gettype ($score). '<br />';
+
+    # 4 Compound Types
+        #array
+        $companies = [1, 2, 3, 0.5, -9.2, 'A', 'b', true];
+        print_r ($companies);
+        #object
+        #callable
+        #interable
+
+    # 2 Special Types
+        #resourse
+        #null
+
+        function sum(float $x, float $y) {
+            $x = 5.5;
+            var_dump($x, $y);
+            echo '<br />';
+            return $x + $y;
+        }
+
+        $sum = sum(2, 3);
+         
+        echo $sum. '<br />';
+
+        var_dump($sum);*/
+
+        $x = (int) '5';
+
+        var_dump($x);
 
         ?>
     
