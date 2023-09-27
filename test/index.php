014796c23a4f4e7c8a37b880bb89768295ca8b0d
AndryRio <andreisprincenatu2@gmail.com>
2023-09-27 17:21:54 +0200

    Float Data Type

--- test/index.php
+++ test/index.php
@@ -99,11 +99,53 @@
          
         echo $sum. '<br />';
 
-        var_dump($sum);*/
+        var_dump($sum);
 
         $x = (int) '5';
 
         var_dump($x);
+
+        // FLOATS
+
+        $x = ceil((0.1 + 0.7) * 10);
+
+        var_dump($x);
+
+        echo $x.'<br />';
+        
+        echo PHP_FLOAT_MAX;
+
+        $y = ceil((0.1 + 0.2) * 10);
+
+        echo $y;
+
+        $x = 0.23;
+        $y = 1 - 0.77;
+
+        var_dump($x, $y);
+
+        if($x == $y) {
+            echo 'Yes';
+        }
+        else {
+            echo 'No';
+        }
+
+        $x = ceil((0.1 + 0.2) * 10);
+
+        //NAN
+        echo log(-1);
+
+        //INF
+        $y = PHP_FLOAT_MAX * 2;
+
+        var_dump(is_finite($y));
+
+        var_dump($y);
+
+        $x = 5;
+
+        var_dump((float) $x);*/
 
         ?>
     
